5a39abb4b7d7926ee0d989ea81d6293d57d52cc5
Axel Dahlberg <git@valleymnt.com>
2021-04-16 11:48:35 +0200

    Use VimEnter event to avoid many calls to register with which-key

--- lua/whichkey_setup.lua
+++ lua/whichkey_setup.lua
@@ -10,8 +10,7 @@ local settings = {
     default_mode = 'n',
 }
 
-local textmaps = {
-}
+local textmaps = {}
 
 -- for debugging
 M.debug = {}
@@ -104,18 +103,7 @@ local function handle_user_opts(opts)
     return split_opts
 end
 
-M.register_keymap = function(initial_key, keymap, opts)
-    opts = handle_user_opts(opts)
-    -- check if initial_key specifies mode for backwards compatibility
-    local mode = opts.mode
-    if initial_key == 'visual' then
-        mode = 'v'
-        initial_key = 'leader'
-    end
-    if initial_key == 'localvisual' then
-        mode = 'v'
-        initial_key = 'localleader'
-    end
+local function get_key_spec(initial_key, mode)
     local key, raw_key
     if (initial_key == 'leader') then -- handle special initial keys
         key = '<Leader>'
@@ -138,21 +126,83 @@ M.register_keymap = function(initial_key, keymap, opts)
         key = initial_key
         raw_key = initial_key
     end
-    local textmap_key = initial_key..'_'..mode
-    if textmaps[textmap_key] == nil then textmaps[textmap_key] = {} end
-    local textmap = textmaps[textmap_key]
-    setup_keymap(mode, {key}, keymap, textmap, opts.keymap_opts, opts.bufnr)
+    return {
+        key = key,
+        raw_key = raw_key,
+    }
+end
 
-    -- TODO at the moment we register with whichkey plugin everytime
-    -- would be better to only do this once but not sure when to do this
-    vim.fn['which_key#register'](raw_key, textmap)
-    raw_key = vim.fn.escape(raw_key, '\\')
-    vim.api.nvim_set_keymap(mode, key, ':<c-u> :WhichKey "'..raw_key..'"<CR>', {silent=true, noremap=true})
+local function to_textmap_key(initial_key, mode, bufnr)
+    if bufnr == nil then
+        bufnr = "x"
+    end
+    return initial_key..' '..mode..' '..bufnr
+end
+
+local function from_textmap_key(textmap_key)
+    local entries = {}
+    for match in  string.gmatch(textmap_key, '%S+') do
+        table.insert(entries, match)
+    end
+    return {
+        initial_key = entries[1],
+        mode = entries[2],
+        bufnr = entries[3],
+    }
 end
 
 local function setup_hide_statusline()
     vim.cmd('autocmd! FileType which_key')
     vim.cmd('autocmd Filetype which_key set laststatus=0 noshowmode noruler | autocmd BufLeave <buffer> set laststatus=2 showmode ruler')
+end
+
+local vim_enter_autocmd_added = false
+local function add_vim_enter_autocmd()
+    vim.cmd('autocmd VimEnter * lua require("whichkey_setup").setup_which_key()')
+    vim_enter_autocmd_added = true
+end
+
+M.setup_which_key = function()
+    for textmap_key, textmap in pairs(textmaps) do
+        local entries = from_textmap_key(textmap_key)
+        local mode = entries.mode
+        local key_spec = get_key_spec(entries.initial_key)
+        local key = key_spec.key
+        local raw_key = key_spec.raw_key
+        -- local bufnr = entries.bufnr -- TODO start using this when supported by which_key
+        vim.fn['which_key#register'](raw_key, textmap)
+        raw_key = vim.fn.escape(raw_key, '\\')
+        vim.api.nvim_set_keymap(mode, key, ':<c-u> :WhichKey "'..raw_key..'"<CR>', {silent=true, noremap=true})
+    end
+end
+
+
+M.register_keymap = function(initial_key, keymap, opts)
+    opts = handle_user_opts(opts)
+    -- check if initial_key specifies mode for backwards compatibility
+    local mode = opts.mode
+    if initial_key == 'visual' then
+        mode = 'v'
+        initial_key = 'leader'
+    end
+    if initial_key == 'localvisual' then
+        mode = 'v'
+        initial_key = 'localleader'
+    end
+
+    local key = get_key_spec(initial_key, mode).key
+    local textmap_key = to_textmap_key(initial_key, mode, opts.bufnr)
+    if textmaps[textmap_key] == nil then textmaps[textmap_key] = {} end
+    local textmap = textmaps[textmap_key]
+    setup_keymap(mode, {key}, keymap, textmap, opts.keymap_opts, opts.bufnr)
+
+    if vim.v.vim_did_enter == 1 then
+        M.setup_which_key()
+    else
+        if not vim_enter_autocmd_added then
+            add_vim_enter_autocmd()
+        end
+    end
 end
 
 M.config = function(user_settings)
