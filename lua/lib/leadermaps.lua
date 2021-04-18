377aaccdfd85523f8343e4848195ce3c1521ee9e
klooj <57016271+klooj@users.noreply.github.com>
2021-04-18 01:34:45 -0400

    Update leadermaps.lua

--- lua/lib/leadermaps.lua
+++ lua/lib/leadermaps.lua
@@ -13,26 +13,22 @@ function maps:load_whichKey_define()
     ["v|<Leader>"] = map_cu("silent WhichKeyVisual '<Space>'"):with_noremap():with_silent(),
     ["n|<LocalLeader>"] = map_cu("silent WhichKey '\\'"):with_noremap():with_silent(),
     ["v|<LocalLeader>"] = map_cu("silent WhichKeyVisual '\\'"):with_noremap():with_silent(),
-    ["n|<LocalLeader>x"] = map_cr("ToggleCheckbox"):with_noremap(),
-    ["n|<Leader>,"] = map_cr("Commentary"):with_noremap():with_silent(),
-    ["n|<Leader><Left>"] = map_cr("BufferMoveNext"):with_noremap():with_silent(),
-    ["n|<Leader>bv"] = map_cmd("<C-W>t<C-W>H"):with_noremap():with_silent(),
-    ["n|<Leader>dl"] = map_cmd("<Plug>(Luadev-RunLine)"),
-    ["n|<Leader>ep"] = map_cr([[vsplit $FOONV/lua/domain/plugins.lua]]):with_noremap(),
-    ["n|<Leader>ff"] = map_cr("lua require('telescope').extensions.fzf_writer.staged_grep()"):with_noremap()
+    ["n|<LocalLeader>x"] = map_cr("ToggleCheckbox"):with_noremap():with_desc("toggle checkbox"),
+    ["n|<Leader>,"] = map_cr("Commentary"):with_noremap():with_silent():with_desc("comment line"),
+    ["n|<Leader><Left>"] = map_cr("BufferMoveNext"):with_noremap():with_silent():with_desc("move to next buffer"),
+    ["n|<Leader>bv"] = map_cmd("<C-W>t<C-W>H"):with_noremap():with_silent():with_desc("arrange windows vertically"),
+    ["n|<Leader>dl"] = map_cmd("<Plug>(Luadev-RunLine)"):with_desc("send line to lua REPL"),
+    ["n|<Leader>ep"] = map_cr([[vsplit $FOONV/lua/domain/plugins.lua]]):with_noremap():with_desc("edit plugins"),
+    ["n|<Leader>ff"] = map_cr("lua require('telescope').extensions.fzf_writer.staged_grep()"):with_noremap():with_desc("staged grep")
   }
 end
 
 local function load_maps()
-  require('wkDescriptions') -- this is a placeholder for the tricky part; see below
   maps:load_whichKey_define()
   binds.nvim_load_mapping(maps.whichKey)
 end
 
 load_maps()
 
--- basically, there just needs to be a wrapper function that will preprocess the which-key setup
--- configuration to parse out the text descriptions, pass them to which-key, and then pass the rest on to this library of
--- functions. but handling keymaps this way, albeit after some more user-friendly tweaking, is more
--- robust in all ways than using whichkey.
+
 ----------------------------------
